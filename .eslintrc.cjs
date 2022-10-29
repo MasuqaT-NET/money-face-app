e9d6d4f3116d6ac9acdc617567085707164b9ae7
MasuqaT <occar@hotmail.co.jp>
2022-10-29 12:10:34 +0900

    Set up tools

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+module.exports = {
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+    extraFileExtensions: [".svelte"],
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["svelte3", "@typescript-eslint"],
+  root: true,
+  overrides: [
+    {
+      files: ["*.svelte"],
+      processor: "svelte3/svelte3",
+    },
+  ],
+  settings: {
+    "svelte3/typescript": true,
+  },
+};
